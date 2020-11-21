9ae03f1aa2ec6fdcd6dc0620d08d17e7c89cea56
bladeofgod <450103187@qq.com>
2020-11-21 21:09:39 +0800

    1.0

---
+++ android/app/src/main/java/com/lijiaqi/flutter_perspective_item/MainActivity.java
@@ -0,0 +1,6 @@
+package com.lijiaqi.flutter_perspective_item;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/demo_page.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/material.dart';
+
+
+class DemoPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return DemoPageState();
+  }
+
+}
+
+class DemoPageState extends State<DemoPage> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Material(
+      child: Container(
+        color: Colors.white,
+        width: size.width,height: size.height,
+        child: ,
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
