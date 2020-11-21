15e36aedcb8135fcb4505837e8e43a0bc9584f0c
bladeofgod <450103187@qq.com>
2020-11-21 21:33:29 +0800

    add image

--- lib/demo_page.dart
+++ lib/demo_page.dart
@@ -11,6 +11,7 @@ class DemoPage extends StatefulWidget{
 }
 
 class DemoPageState extends State<DemoPage> {
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -18,8 +19,21 @@ class DemoPageState extends State<DemoPage> {
       child: Container(
         color: Colors.white,
         width: size.width,height: size.height,
-        child: ,
+        child: Image.asset('assets/timg.jpg',
+          cacheWidth: 400,cacheHeight: 400,
+        ),
       ),
+    );
+  }
+
+  Widget listView(Size size){
+    return ListView(
+      children: List.generate(30, (index){
+        return Container(
+          width: size.width,height: size.height/6,
+          color: index % 2 == 0 ? Colors.blue : Colors.red,
+        );
+      }),
     );
   }
 }
